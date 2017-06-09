4e3db4533a793c7661de2f8164e36e591507e034
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2017-06-09 12:27:27 +0200

    add one more test class

---
+++ NUnitStopOnError/FirstProject/FailingTests2.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace FirstProject
+{
+    [TestFixture]
+    public class FailingTests2
+    {
+        [Test]
+        public void ShouldPassTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
